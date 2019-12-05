f6533890ae331c6ed377c28ae17d2687639751c1
Kai Averkvist <36230927+kaiaverkvist@users.noreply.github.com>
2019-12-05 16:35:47 +0100

    Update discordmessage.go

--- discordmessage.go
+++ discordmessage.go
@@ -4,30 +4,34 @@ type DiscordMessage struct {
 	Username  string `json:"username"`
 	AvatarURL string `json:"avatar_url"`
 	Content   string `json:"content"`
-	Embeds    []struct {
-		Author struct {
-			Name    string `json:"name"`
-			URL     string `json:"url"`
-			IconURL string `json:"icon_url"`
-		} `json:"author"`
-		Title       string `json:"title"`
-		URL         string `json:"url"`
-		Description string `json:"description"`
-		Color       int    `json:"color"`
-		Fields      []struct {
-			Name   string `json:"name"`
-			Value  string `json:"value"`
-			Inline bool   `json:"inline,omitempty"`
-		} `json:"fields"`
-		Thumbnail struct {
-			URL string `json:"url"`
-		} `json:"thumbnail"`
-		Image struct {
-			URL string `json:"url"`
-		} `json:"image"`
-		Footer struct {
-			Text    string `json:"text"`
-			IconURL string `json:"icon_url"`
-		} `json:"footer"`
-	} `json:"embeds"`
+	Embeds    []DiscordEmbed `json:"embeds"`
+}
+
+type DiscordEmbed struct {
+	Author struct {
+		Name    string `json:"name"`
+		URL     string `json:"url"`
+		IconURL string `json:"icon_url"`
+	} `json:"author"`
+	Title       string `json:"title"`
+	URL         string `json:"url"`
+	Description string `json:"description"`
+	Color       int    `json:"color"`
+	Fields      []DiscordEmbedField `json:"fields"`
+	Thumbnail struct {
+		URL string `json:"url"`
+	} `json:"thumbnail"`
+	Image struct {
+		URL string `json:"url"`
+	} `json:"image"`
+	Footer struct {
+		Text    string `json:"text"`
+		IconURL string `json:"icon_url"`
+	} `json:"footer"`
+}
+
+type DiscordEmbedField struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline,omitempty"`
 }
